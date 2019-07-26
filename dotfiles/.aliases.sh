dcd8a819334aff916ff3699c34b1f984d12d59ed
Richie Thomas <rickthomas1980@gmail.com>
2019-07-26 15:28:34 -0700

    Add "be", "sbp", and "acki" to aliases dotfile
    -"be" runs `bundle exec` 
    -"sbp" runs "source ~/.bash_profile"
    -"acki" runs "ack --ignore-dir={tmp,log} $@"

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -3,3 +3,7 @@ alias ber='bundle exec rspec'
 function gitb() {
   git blame --date=short $1 | sed -e 's, [^(]*, ,' -e 's, [^ ]* \([0-9]*)\), \1,'
 }
+alias be='bundle exec'
+alias sbp='source ~/.bash_profile'
+alias acki='ack --ignore-dir={tmp,vendor,log} $@'
+
