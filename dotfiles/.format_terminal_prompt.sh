a5a5a5e66891bc6224eb1c370955eb607aa1a167
Richie Thomas <rickthomas1980@gmail.com>
2019-08-02 15:42:17 -0700

    Fix bug in shell prompt
    Bug => displayed branch name wouldn't change when actual branch changed

--- dotfiles/.format_terminal_prompt.sh
+++ dotfiles/.format_terminal_prompt.sh
@@ -2,4 +2,4 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-PS1="\[\e[36m\](\W)$(parse_git_branch) \[\e[m\]$ "
+PS1="\[\e[36m\](\W)\$(parse_git_branch) \[\e[m\]$ "
