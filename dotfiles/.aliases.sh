c94e1c29ed9fcbcfa0c22730bc05524f28ab9bf7
Richie Thomas <rickthomas1980@gmail.com>
2019-08-12 10:27:53 -0700

    Added "alias berof='bundle exec rspec --only-failures'"

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -1,11 +1,15 @@
-alias ber='bundle exec rspec'
-
 function gitb() {
   git blame --date=short $1 | sed -e 's, [^(]*, ,' -e 's, [^ ]* \([0-9]*)\), \1,'
 }
+
 alias be='bundle exec'
+alias ber='bundle exec rspec'
+alias berof='bundle exec rspec --only-failures'
+
 alias sbp='source ~/.bash_profile'
+
 alias acki='ack --ignore-dir={tmp,vendor,log} $@'
+
 alias gpr='git pull --rebase'
 alias gco='git checkout $@'
 alias gno='git show --name-only $@'
